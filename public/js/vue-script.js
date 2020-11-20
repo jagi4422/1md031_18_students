a7507d4a5edfbd4a4519cedb081345710fbd9b5e
jagi4422 <jakob.gimbringer.4422@student.uu.se>
2020-11-20 20:09:55 +0100

    simple formatting

--- public/js/vue-script.js
+++ public/js/vue-script.js
@@ -7,7 +7,7 @@ var vm = new Vue({
     burgers: food,
     orders: {},
     ordersList: {},
-  },  
+  },
 
   created: function () {
     socket.on('initialize', function (data) {
@@ -24,45 +24,47 @@ var vm = new Vue({
     },
 
     addOrder: function () {
-      
+
       this.ordersList[0].orderId = this.getNext();
       this.ordersList[0].orderItems = returnBurgerSelection();
       this.ordersList[0].customerInfo = returnOrderInfo();
-      
+
       console.log(this.ordersList[0].orderItems);
       console.log(this.ordersList[0].orderId);
       console.log(this.ordersList[0]);
 
-      socket.emit("addOrder",this.ordersList[0]);
+      socket.emit("addOrder", this.ordersList[0]);
       console.log("efter")
     },
-    
+
     displayOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-        y: event.currentTarget.getBoundingClientRect().top};
-      
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
+
       var newOrder = {
-        
+
         orderId: undefined,
-        
+
         details: {
-        x: event.clientX - 10 - offset.x,
-        y: event.clientY - 10 - offset.y
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y
         },
 
-        orderItems:[],
+        orderItems: [],
 
-        customerInfo:{},
-        };
+        customerInfo: {},
+      };
 
-        this.ordersList = [newOrder];
+      this.ordersList = [newOrder];
 
     },
 
 
-    buttonClicked: function(){
-      outputOrder(new Customer("name","email","gender","payment"), this.ordersList[0].details);  
-      this.addOrder();    
+    buttonClicked: function () {
+      outputOrder(new Customer("name", "email", "gender", "payment"), this.ordersList[0].details);
+      this.addOrder();
     },
   }
 })
